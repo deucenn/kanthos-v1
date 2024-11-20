0fedaf1b1851adf039e19994325b1dd7872cd9d4
deucenn <panix.black@web.de>
2024-11-20 17:22:08 +0100

    API integrated

--- src/app/components/ProductsContainer.js
+++ src/app/components/ProductsContainer.js
@@ -1,11 +1,52 @@
+"use client"
+
+import { useEffect, useState } from "react";
+import Image from "next/image";
+
 export default function ProductsContainer() {
-    const products = async () => {
-        try {
-            const res = await fetch("https://api.printify.com/v1/shops/:shop_id/products.json");
-            const data = await res.json();
-            console.log(data)
-        } catch (err) {
-            console.error(err);
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const res = await fetch("https://kanthos-backend.onrender.com/api/products");
+        const printifyData = await res.json();
+
+        if (printifyData?.data?.length > 0) {
+          setProducts(printifyData.data);
         }
-    }
+      } catch (err) {
+        console.error("Error fetching products:", err);
+        setError("Failed to load products.");
+      }
+    };
+
+    fetchProducts();
+  }, []); 
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  return (
+    <div>
+      {products.length > 0 ? (
+        products.map((product) => (
+          <div key={product.id}>
+            <h2>{product.title}</h2>
+            <Image
+              src={product.images[0]?.src}
+              alt={product.name || "Product Image"}
+              width={300}
+              height={300}
+            />
+            <p>{product.external?.handle}</p>
+          </div>
+        ))
+      ) : (
+        <p>Loading products...</p>
+      )}
+    </div>
+  );
 }
--- src/app/products/page.js
+++ src/app/products/page.js
@@ -1,10 +1,12 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import ProductsContainer from "../components/ProductsContainer";
 
 export default function ProductsPage() {
   return (
     <div className="flex flex-col justify-center items-center h-screen">
       <Navbar />
+      <ProductsContainer />
       <Footer />
     </div>
   );
